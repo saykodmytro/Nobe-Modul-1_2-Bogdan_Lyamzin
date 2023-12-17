dd83b079f20fde1a9a3f89130c3e8f0b1f6e4dfb
Dmytro Sayko <saykodmytro@icloud.com>
2023-12-18 00:03:15 +0200

    up

--- work-with-json-files/app.js
+++ work-with-json-files/app.js
@@ -1,4 +1,4 @@
-const books = require("./books");
+const books = require("./books"); // імпорт іде зі папки books але насправді він іде від папки index.js. З index.js експортуються методи для роботи з цими книгами
 // console.log("books: ", books);
 
 const invokeAction = async ({ action, id, title, author }) => {
@@ -6,23 +6,28 @@ const invokeAction = async ({ action, id, title, author }) => {
     case "read":
       const allBooks = await books.getAll();
       return console.log(allBooks);
-    // case "getById":
-    //   const oneBook = await books.getById(id);
-    //   return console.log(oneBook);
-    // case "add":
-    //   const newBook = await books.add({ title, author });
-    //   return console.log(newBook);
-    // case "updateById":
-    //   const updateBook = await books.updateById(id, { title, author });
-    //   return console.log(updateBook);
-    // case "deleteById":
-    //   const deleteBook = await books.deleteById(id);
-    //   return console.log(deleteBook);
+    case "getById":
+      const oneBook = await books.getById(id);
+      return console.log(oneBook);
+    case "add":
+      const newBook = await books.add({ title, author });
+      return console.log(newBook);
+    case "updateById":
+      const updateBook = await books.updateById(id, { title, author });
+      return console.log(updateBook);
+    case "deleteById":
+      const deleteBook = await books.deleteById(id);
+      return console.log(deleteBook);
   }
 };
 
 // invokeAction({ action: "read" });
-// invokeAction({action: "getById", id: "u9kgwNWGi3uUUwh0b8V49"});
-// invokeAction({action: "add", title: "Worm", author: "John C. McCrae"});
-// invokeAction({action: "updateById", id: "uNrjVoY1Ll0GFOof6t4yR", title: "Ward", author: "John C. McCrae"});
-// invokeAction({action: "deleteById", id: "uNrjVoY1Ll0GFOof6t4yR"});
+// invokeAction({ action: "getById", id: "3" });
+// invokeAction({ action: "add", title: "Worm", author: "John C. McCrae" });
+// invokeAction({
+//   action: "updateById",
+//   id: "8",
+//   title: "XXX",
+//   author: "XXXXXX",
+// });
+invokeAction({ action: "deleteById", id: "6" });
--- work-with-json-files/books/index.js
+++ work-with-json-files/books/index.js
@@ -1,59 +1,63 @@
-const fs = require("fs/promises");
-const path = require("path");
+const fs = require("fs/promises"); // для роботи з файлами використовується пакет fs , але щоб зручно було працювати з async await його імпортують із папки /promises
+const path = require("path"); // за допомог пакету path створуємо шляхи, він вбудований в node його не потрібно встановлювати
 // const { nanoid } = require("nanoid");
 
-const booksPath = path.join(__dirname, "books.json");
+const booksPath = path.join(__dirname, "books.json"); // path має метод join який обєднує шляхи, нормалізує шлях
 
-console.log(__dirname);
+const { nanoid } = require("nanoid");
 
 const getAll = async () => {
-  const data = await fs.readFile(booksPath, "utf-8");
-  return JSON.parse(data);
+  const data = await fs.readFile(booksPath);
+  return JSON.parse(data); // необхідно пам'ятати, що параметр data у функції readFile містить вже згадуваний об'єкт типу Buffer містить послідовність прочитаних байтів, тобто сирі дані. Але JSON.parse також перетворює Buffer на масив обєктів
 };
 
-// const getById = async (id) => {
-//   const books = await getAll();
-//   const result = books.find((item) => item.id === id);
-//   return result || null;
-// };
+// для отримання однієї книги по id ми використовуємо нашу ф-ю getAll запизуємо її результат і методом find знаходимо нашу книгу
+const getById = async (id) => {
+  const books = await getAll();
+  const result = books.find((item) => item.id === id);
+  return result || null;
+};
 
-// const add = async (data) => {
-//   const books = await getAll();
-//   const newBook = {
-//     id: nanoid(),
-//     ...data,
-//   };
-//   books.push(newBook);
-//   await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
-//   return newBook;
-// };
+// для додавання книги робимо наступне: спочатку створюємо нову книгу на основі наших даних (розпилюємо і додаємо id), потім витягуємо всі наші книги і пушимо нашу нову книгу, дальше перепизуємо нашу книгу і , щоб коректно записалось не в одну строку додаємо опції.
+// В stringify можна передати три аргумента, перший наш обєкт , другий символи заміни - якщо під час перетворення масива або обєкта нам треба один символ замінити на інший ми їх пишемо другим аргументом. Третій аргумент - відступи, якщо вказати 1 або більше то stringify запише не в одну строку а нормально
+const add = async (data) => {
+  const newBook = {
+    id: nanoid(),
+    ...data,
+  };
+  const books = await getAll();
+  books.push(newBook);
+  await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
+  return newBook;
+};
 
-// const updateById = async (id, data) => {
-//   const books = await getAll();
-//   const index = books.findIndex((item) => item.id === id);
-//   if (index === -1) {
-//     return null;
-//   }
-//   books[index] = { id, ...data };
-//   await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
-//   return books[index];
-// };
+const updateById = async (id, data) => {
+  const books = await getAll();
+  const index = books.findIndex((item) => item.id === id);
+  console.log("index: ", index);
+  if (index === -1) {
+    return null;
+  }
+  books[index] = { id, ...data };
+  await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
+  return books[index];
+};
 
-// const deleteById = async (id) => {
-//   const books = await getAll();
-//   const index = books.findIndex((item) => item.id === id);
-//   if (index === -1) {
-//     return null;
-//   }
-//   const [result] = books.splice(index, 1);
-//   await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
-//   return result;
-// };
+const deleteById = async (id) => {
+  const books = await getAll();
+  const index = books.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = books.splice(index, 1);
+  await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
+  return result;
+};
 
 module.exports = {
   getAll,
-  //   getById,
-  //   add,
-  //   updateById,
-  //   deleteById,
+  getById,
+  add,
+  updateById,
+  deleteById,
 };
